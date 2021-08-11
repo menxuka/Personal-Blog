b3e06ebe089c012df7e291e55c37ba2c15ac911d
menxuka <menuka.tasnim.nima@g.bracu.ac.bd>
2021-08-12 01:03:04 +0600

    backend

---
+++ blog-backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const adminRoute = require('./routes/admins');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
+    .then(console.log('Connected'))
+    .catch((error) => console.log(error));
+
+app.use('/blog-backend/auth', authRoute);
+app.use('/blog-backend/admins', adminRoute);
+app.use('/blog-backend/posts', postRoute);
+app.use('/blog-backend/categories', categoryRoute);
+
+app.use('/', (req, res) => {
+    console.log('main url');
+})
+
+app.listen('5000', () => {
+    console.log('backend running');
+})
